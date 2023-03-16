b39c21d4f1578ef2c4ad809159ac7656b0ba129a
Eduardo Alfonso Acevedo Rodriguez <e_acevedo+SEG@seg.guanajuato.gob.mx>
2023-03-16 15:31:03 -0600

    Prueba

---
+++ controladores/Api.php
@@ -0,0 +1,19 @@
+<?php
+
+class Home extends App
+{
+
+    function __construct()
+    {
+    }
+
+    public function getUsuario()
+    {
+         echo "Hola";
+    }
+    public function getUsuarios()
+    {
+         
+    }
+ 
+}
